81c4af427b71ef947b7e3ee9abba857708fa2a0b
Marcin Owerczuk <muzieh@gmail.com>
2016-12-14 22:45:39 +0000

    specflow added

---
+++ Spec/CalculateAdditionSteps.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Spec
+{
+	[Binding]
+	public class CalculateAdditionSteps
+	{
+		private Calculator _calculator = new Calculator();
+
+		[Given(@"I have entered (.*) into the calculator")]
+		public void GivenIHaveEnteredIntoTheCalculator(int p0)
+		{
+			_calculator.Enter(p0);
+		}
+
+		[Given(@"I press add")]
+		public void GivenIPressAdd()
+		{
+			_calculator.Add();
+		}
+
+
+		[When(@"I press equals")]
+		public void WhenIPressEquals()
+		{
+			_calculator.Eqls();
+		}
+
+		[Then(@"the result should be (.*) on the screen")]
+		public void ThenTheResultShouldBeOnTheScreen(int p0)
+		{
+			Assert.AreEqual(p0, _calculator.Result());
+		}
+
+		[Then(@"result (.*) on the screen")]
+		public void ThenResultOnTheScreen(int p0)
+		{
+			Assert.AreEqual(p0, _calculator.Result()); ;
+		}
+
+
+
+
+	}
+
+	public class Calculator
+
+	{
+		private List<int> numbers = new List<int>();
+
+		public void Add()
+		{
+		}
+
+		public void Enter(int number)
+		{
+			numbers.Add(number);
+		}
+
+		public void Eqls()
+		{
+		}
+
+		public int Result()
+		{
+			return numbers.Sum();
+
+		}
+	}
+}
---
+++ Spec/Features/CalculateAddition.feature
@@ -0,0 +1,32 @@
+﻿Feature: CalculateAddition
+	In order speed up calculation 
+	As a user
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I press add
+	And I have entered 70 into the calculator
+	When I press equals
+	Then the result should be 120 on the screen
+
+Scenario: Add positive and negative numbers
+	Given I have entered 50 into the calculator
+	And I press add 
+	And I have entered -20 into the calculator
+	When I press equals
+	Then result 30 on the screen
+
+Scenario Outline: Add more numbers
+	Given I have entered <ValueA> into the calculator
+	And I press add 
+	And I have entered <ValueB> into the calculator
+	When I press equals
+	Then the result should be <Result> on the screen
+
+Examples: 
+| ValueA | ValueB | Result  |
+| 10     | 20     | 30      |
+| 1      | 4      | 5       |
+| -8     | 8      | 0       |
